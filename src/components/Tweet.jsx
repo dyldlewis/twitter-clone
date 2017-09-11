50e248de81ddfe2b65626c973b4d11806e604599
Max Scher and Nathan Stewart <max@>
2017-09-11 15:30:49 -0700

    implement hard-coded tweets within tweetFeed

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function Tweet(props) {
+  var imgStyle = {
+    maxWidth: "50",
+    display: "inline-block",
+    margin: "10",
+    marginLeft: "20"
+  }
+  var tweetStyle = {
+    marginLeft: "20",
+    display: "inline-block",
+    width: "80%"
+  }
   return(
-    <h3>Individual Tweet</h3>
+    <div>
+      <img style={imgStyle} src={props.img}/>
+      <div style={tweetStyle}>
+        <h3>{props.title}</h3>
+        <p>{props.content}</p>
+      </div>
+      <hr/>
+    </div>
   );
+}
+
+Tweet.propTypes = {
+  img: PropTypes.string,
+  title: PropTypes.string,
+  content: PropTypes.string
 }
 
 export default Tweet;
--- src/components/TweetFeed.jsx
+++ src/components/TweetFeed.jsx
@@ -4,8 +4,21 @@ import Tweet from './Tweet';
 function TweetFeed(props) {
   return (
     <div>
-      <h2>Tweets</h2>
-      <Tweet/>
+      <Tweet
+        img="http://www.iconsdb.com/icons/preview/gray/twitter-xxl.png"
+        title="OMG"
+        content="He's just not that into you, Margaret."
+      />
+      <Tweet
+        img="https://didatticattivablog.files.wordpress.com/2016/12/bbe0fbe844c72eea190bf8b51b401be0.jpg"
+        title="Why doesn't he like me?"
+        content="Philip is so freaking cute. Why won't he ever notice me, huh ladies?!?"
+      />
+      <Tweet
+        img="http://www.iconsdb.com/icons/preview/gray/twitter-xxl.png"
+        title="Philip"
+        content="What a dreamboat"
+      />
     </div>
   );
 }
