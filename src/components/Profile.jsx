a5861216d08733e06cdc24880fc4167df0a25706
Max Scher and Nathan Stewart <max@>
2017-09-11 11:17:30 -0700

    finalize profile component styling

---
+++ src/components/Profile.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+function Profile(props) {
+  var profileBoxStyle = {
+    textAlign: "left",
+    border: "lightgrey 1px solid",
+    display: "inline-block",
+    overflow: "hidden",
+    padding: "10"
+  }
+  var listStyle = {
+    listStyle: "none",
+    paddingLeft: "0"
+  }
+  var listItemStyle = {
+    display: "inline",
+    padding: "5",
+    listStylePosition: "outside",
+    color: "grey",
+    fontSize: "12"
+  }
+  var imgStyle = {
+    maxWidth: "75",
+    borderRadius: "10",
+  }
+  var nameStyle = {
+    float: "right",
+    marginRight: "7",
+    marginTop: "50"
+  }
+  return(
+    <div style={profileBoxStyle}>
+      <img style={imgStyle} src="http://icons.veryicon.com/png/Internet%20%26%20Web/Simple%201/twitter.png"/>
+      <h3 style={nameStyle}>Nancy Kerrigan</h3>
+      <ul style={listStyle}>
+        <li style={listItemStyle}>TWEETS</li>
+        <li style={listItemStyle}>FOLLOWING</li>
+        <li style={listItemStyle}>FOLLOWERS</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Profile;
