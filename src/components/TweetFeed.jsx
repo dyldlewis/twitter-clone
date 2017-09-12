d169e6882a5d4946600e97dd2ec2254dbf542306
Dylan D. Lewis and Max R. Scher <max@>
2017-09-12 08:28:00 -0700

    refactor TweetFeed to loop through hard-coded tweetList array

--- src/components/TweetFeed.jsx
+++ src/components/TweetFeed.jsx
@@ -1,24 +1,33 @@
 import React from 'react';
 import Tweet from './Tweet';
 
+var masterTweetList = [
+  {
+    img: "http://www.iconsdb.com/icons/preview/gray/twitter-xxl.png",
+    title: "OMG",
+    content: "He's just not that into you, Margaret."
+  },
+  {
+    img: "https://didatticattivablog.files.wordpress.com/2016/12/bbe0fbe844c72eea190bf8b51b401be0.jpg",
+    title: "Why doesn't he like me?",
+    content: "Philip is so freaking cute. Why won't he ever notice me, huh ladies?!?"
+  },
+  {
+    img: "http://www.iconsdb.com/icons/preview/gray/twitter-xxl.png",
+    title: "Philip",
+    content: "What a dreamboat"
+  }
+]
+
 function TweetFeed(props) {
   return (
     <div>
-      <Tweet
-        img="http://www.iconsdb.com/icons/preview/gray/twitter-xxl.png"
-        title="OMG"
-        content="He's just not that into you, Margaret."
-      />
-      <Tweet
-        img="https://didatticattivablog.files.wordpress.com/2016/12/bbe0fbe844c72eea190bf8b51b401be0.jpg"
-        title="Why doesn't he like me?"
-        content="Philip is so freaking cute. Why won't he ever notice me, huh ladies?!?"
-      />
-      <Tweet
-        img="http://www.iconsdb.com/icons/preview/gray/twitter-xxl.png"
-        title="Philip"
-        content="What a dreamboat"
-      />
+      {masterTweetList.map((tweet, index) =>
+        <Tweet img={tweet.img}
+               title={tweet.title}
+               content={tweet.content}
+               key={index}/>
+      )}
     </div>
   );
 }
